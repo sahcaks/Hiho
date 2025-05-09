5f40192f2e41cb62eef39abd52014bad239d56be
Navitski_aliaksei <leha.nov98@gmail.com>
2025-05-09 14:42:48 +0300

    remove admin config

--- admin/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-const PROJECT_NAME = 'Hiho';
-const MAIN_URL = "http://localhost:8888/";
-const ADMIN_URL = "/hiho/admin";
-
-const PAGE_404 = '/pages/errors/404.php';
-
-const URL_MAIN_IMG = "/hiho/img";
-const ADMIN_FRONT_URL = MAIN_URL . "hiho/admin/front/";
--- pages/errors/500.php
+++ pages/errors/500.php
@@ -1,4 +1,4 @@
-<?php require_once __DIR__ . '/../../admin/config.php'?>
+<?php require_once dirname(__DIR__) . '/../config/config.php' ?>
 <!DOCTYPE html>
 <html lang="en">
 
